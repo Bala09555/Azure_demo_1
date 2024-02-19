92015ec80362b2676a8409f565fd9b31550a3cf5
Bala09555 <balanagireddy1997@gmail.com>
2024-02-19 23:53:29 +0530

    SEC_commit

--- src/test/java/com/integrationninjas/springbootexample/SpringbootExampleApplicationTests.java
+++ src/test/java/com/integrationninjas/springbootexample/SpringbootExampleApplicationTests.java
@@ -1,6 +1,6 @@
 package com.integrationninjas.springbootexample;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
